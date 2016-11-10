57dea73bc6be406235e60697eceb69ec2fb921f6
Alexei <CityFromTheAsh@gmail.com>
2016-11-10 18:20:11 +0200

    10.11.2016 Last. Design BookShoop almost done

--- db/migrate/20161110125228_add_filds_to_user.rb
+++ db/migrate/20161110125228_add_filds_to_user.rb
@@ -1,5 +1,4 @@
 class AddFildsToUser < ActiveRecord::Migration[5.0]
   def change
-    
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161110124613) do
+ActiveRecord::Schema.define(version: 20161110125228) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
