2c993b7680e082179e38e6c5f5269a835c4ba523
Alexei <CityFromTheAsh@gmail.com>
2016-11-16 14:18:52 +0200

    Add Reference To User

---
+++ db/migrate/20161116120246_add_reference_to_books_from_user.rb
@@ -0,0 +1,5 @@
+class AddReferenceToBooksFromUser < ActiveRecord::Migration[5.0]
+  def change
+    add_reference :users, :books, index: true, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161116115550) do
+ActiveRecord::Schema.define(version: 20161116120246) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -57,6 +57,8 @@ ActiveRecord::Schema.define(version: 20161116115550) do
     t.boolean  "admin"
     t.decimal  "balance",                default: "0.0", null: false
     t.string   "login",                  default: "",    null: false
+    t.integer  "books_id"
+    t.index ["books_id"], name: "index_users_on_books_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
