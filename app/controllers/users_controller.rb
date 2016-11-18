f9591abd871019bcbc3330facda3ac7107404eb8
Alexei <CityFromTheAsh@gmail.com>
2016-11-18 14:49:44 +0200

    Update user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,13 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show]
+  before_action :set_user, only: [:show, :edit]
   require 'carrierwave/orm/activerecord'
 
   def index
     @users = User.all
     @users = @users.page(params[:page])
+  end
+
+  def edit
   end
 
   def show
@@ -19,6 +22,17 @@ class UsersController < ApplicationController
   def current_lots
   end
 
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'Book was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
   private
   def set_user
     @user = User.find(params[:id])
