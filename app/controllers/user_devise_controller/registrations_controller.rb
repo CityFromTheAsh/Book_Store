572df70a0636c09ee6b508af07f56872e5493dc2
Alexei <CityFromTheAsh@gmail.com>
2016-11-18 11:43:06 +0200

    Add downloading Avatar

--- app/controllers/user_devise_controller/registrations_controller.rb
+++ app/controllers/user_devise_controller/registrations_controller.rb
@@ -40,12 +40,12 @@ class UserDeviseController::RegistrationsController < Devise::RegistrationsContr
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:login])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:login, :avatar])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:login])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:login, :avatar])
   end
 
   # The path used after sign up.
