30ccc65997b929fc4dd6849602de419db3337678
Alexei <CityFromTheAsh@gmail.com>
2016-11-30 11:36:29 +0200

    Remove unuseble function

--- app/assets/javascripts/books.js
+++ app/assets/javascripts/books.js
@@ -1,10 +1,3 @@
 $(document).ready(function(){
-    console.log('ready')
-    $('.menu').click(function () {
-        console.log('clicked')
-        $(this).next().toggle();
-    });
-});
-$(document).ready(function(){
     $('.accordion-menu').accordion();
 });
