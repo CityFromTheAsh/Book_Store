6b8736549fb60c980834a6efa8ac710ee3d97933
Alexei <CityFromTheAsh@gmail.com>
2016-12-02 14:44:40 +0200

    Add seeds new admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+a = User.new()
+a.login = 'Weed15'
+a.email = 'AdminCityFromTheAsh@gmail.com'
+a.password = '48744874'
+a.admin = true
+a.ban = false
+a.save
