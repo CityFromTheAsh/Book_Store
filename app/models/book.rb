34a69d239796ed361ea3615516a9560bfde19871
Alexei <CityFromTheAsh@gmail.com>
2017-02-23 17:51:39 +0200

    Feature books.index translate

--- app/models/book.rb
+++ app/models/book.rb
@@ -7,7 +7,8 @@ class Book < ApplicationRecord
   has_one :order
   accepts_nested_attributes_for :images
 
-  enumerize :binding, in: {hardcover: true, softcover: false}
+  enumerize :binding, in: {hardcover: true, softcover: false} if I18n.locale == :en
+  enumerize :binding, in: {твёрдый: true, мягкий: false} if I18n.locale == :ru
   enumerize :status, in: %w(for_sale payment delivery control sold banned)
 
   paginates_per 5
