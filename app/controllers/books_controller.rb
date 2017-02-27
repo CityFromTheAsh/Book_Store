0a3fac9cd0456df975d195ea3d54dd0cd4c243b3
Alexei <CityFromTheAsh@gmail.com>
2017-02-27 13:37:13 +0200

    Add order as action

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -95,6 +95,12 @@ class BooksController < ApplicationController
     end
   end
 
+  def order
+    @book = Book.find(params[:book_id])
+    @book.update(status: :payment)
+    redirect_to books_path
+  end
+
   private
   # Use callbacks to share common setup or constraints between actions.
   def set_book
